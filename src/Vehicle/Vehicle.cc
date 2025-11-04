44689c9312c990b957e5e0d8542f8c9667e5cdab
Derker2002 <siklerzp@gmail.com>
2025-11-04 12:56:41 +0100

    separete GLOBAL_POS_INT and GPS_RAW_INT paths

--- src/Vehicle/Vehicle.cc
+++ src/Vehicle/Vehicle.cc
@@ -742,8 +742,9 @@ void Vehicle::_handleGpsRawInt(mavlink_message_t& message)
     _gpsRawIntMessageAvailable = true;
 
     if (gpsRawInt.fix_type >= GPS_FIX_TYPE_3D_FIX) {
+        QGeoCoordinate newPosition(gpsRawInt.lat  / (double)1E7, gpsRawInt.lon / (double)1E7, gpsRawInt.alt  / 1000.0);
+        emit updateTrajectory(newPosition,eSrc_GPSRaw);
         if (!_globalPositionIntMessageAvailable) {
-            QGeoCoordinate newPosition(gpsRawInt.lat  / (double)1E7, gpsRawInt.lon / (double)1E7, gpsRawInt.alt  / 1000.0);
             if (newPosition != _coordinate) {
                 _coordinate = newPosition;
                 emit coordinateChanged(_coordinate);
@@ -761,6 +762,8 @@ void Vehicle::_handleGlobalPositionInt(mavlink_message_t& message)
     mavlink_global_position_int_t globalPositionInt;
     mavlink_msg_global_position_int_decode(&message, &globalPositionInt);
 
+
+
     if (!_altitudeMessageAvailable) {
         _altitudeRelativeFact.setRawValue(globalPositionInt.relative_alt / 1000.0);
         _altitudeAMSLFact.setRawValue(globalPositionInt.alt / 1000.0);
@@ -777,6 +780,7 @@ void Vehicle::_handleGlobalPositionInt(mavlink_message_t& message)
     if (newPosition != _coordinate) {
         _coordinate = newPosition;
         emit coordinateChanged(_coordinate);
+        emit updateTrajectory(newPosition,eSrc_GlobalPosition);
     }
 }
 
