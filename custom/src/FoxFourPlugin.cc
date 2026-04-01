32d26a9304847ca4ca344f6c794f88aa6dc6ce82
Derker2002 <siklerzp@gmail.com>
2026-04-01 19:18:22 +0200

    remove spamming message

--- custom/src/FoxFourPlugin.cc
+++ custom/src/FoxFourPlugin.cc
@@ -46,7 +46,8 @@ bool FoxFourPlugin::overrideSettingsGroupVisibility(const QString& name) {
 
 VideoReceiver* FoxFourPlugin::createVideoReceiver(QObject* parent) {
 #ifdef QGC_GST_STREAMING
-    return new FoxFourGstVideoReceiver(parent);
+    return QGCCorePlugin::createVideoReceiver(parent);
+    // return new FoxFourGstVideoReceiver(parent);
 #elif defined(QGC_QT_STREAMING)
     return QtMultimediaReceiver::createVideoReceiver(parent);
 #else
